a0559785f8f7829e72e51d53bd9c848afeb59e15
seungin <tmddls91@inha.edu>
2025-01-30 03:44:57 +0900

    [feat] admin 계정 생성 및 등록 완료

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'nickname', 'user_age', 'user_gender', 'created_at')
+    search_fields = ('email', 'nickname')  # 검색 기능 추가
+    list_filter = ('user_gender', 'created_at')  # 필터 기능 추가
+    readonly_fields = ('created_at',)  # 생성일자는 수정 불가능하게 설정
