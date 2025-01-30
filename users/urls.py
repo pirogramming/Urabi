e515a6423f2917d66428ff04e64759c98f0193fa
03minseok <iris15977@gmail.com>
2025-01-30 21:33:06 +0900

    [feat] accompany_list.html 구현

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -15,4 +15,5 @@ urlpatterns = [
     path('login/naver/', views.naver_login, name='naver-login'),
     path('login/naver/callback/', views.naver_login_callback, name='naver-callback'),
     path('logout/', views.user_logout, name='logout'),
+    path('accompany/', include('accompany.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
