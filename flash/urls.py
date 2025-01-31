d694889eee582393ef4453a5d076b1c04b19d226
dhrkawk <jajwon202@naver.com>
2025-01-31 18:55:59 +0900

    [feat] flash list html 구현완료, flash register html 초안

--- flash/urls.py
+++ flash/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import flash_list
+from .views import *
 
 urlpatterns = [
     path('list/', flash_list, name='flash_list'),
+    path("register/", flash_register, name="flash_register"),
 ]
--- flash/views.py
+++ flash/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def flash_list(request):
     return render(request, 'flash/flash_list.html')
+
+def flash_register(request):
+    return render(request, "flash/flash_register.html")
