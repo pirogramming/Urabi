cc3a82ae54326cae8a0329ce13fb4d50f1fa967f
iseun15 <ave0315@naver.com>
2025-02-02 03:03:37 +0900

    [feat]나눔 마켓 페이지 연결

--- market/urls.py
+++ market/urls.py
@@ -6,5 +6,7 @@ app_name = 'market'
 
 urlpatterns = [
     path('', views.market_list, name='market_list'),
-    
+    path('create', views.market_create, name='market_create'),
+    path('detail_guest', views.market_detail_guest, name='market_detail_guest'),
+    path('detail_self', views.market_detail_self, name='market_detail_self'),
 ]
--- market/views.py
+++ market/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def market_list(request):
     return render(request, 'market/market_list.html')
+
+
+def market_create(request):
+    return render(request, 'market/market_create.html')
+
+def market_detail_guest(request):
+    return render(request, 'market/market_detail_guest.html')
+
+def market_detail_self(request):
+    return render(request, 'market/market_detail_self.html')
