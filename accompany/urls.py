496701041fac55344f44d432f35129d6b294e7f4
03minseok <iris15977@gmail.com>
2025-02-02 05:48:28 +0900

    [feat] 동행 모집 삭제 구현

--- accompany/urls.py
+++ accompany/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.AccompanyListView.as_view(), name='accompany_list'),
     path('detail/<int:pk>', views.AccompanyDetailView.as_view(), name='accompany_detail'),
     path('create/', views.AccompanyCreateView.as_view(), name='accompany_create'),
+    #path('update/<int:pk>', views.AccompanyUpdateView.as_view(), name='accompany_update'),
+    path('delete/<int:pk>', views.AccompanyDeleteView.as_view(), name='accompany_delete'),
 ]
--- accompany/views.py
+++ accompany/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse
 from . import views
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import TravelGroup, TravelParticipants
 from .forms import TravelGroupForm
 
@@ -27,3 +27,8 @@ class AccompanyCreateView(CreateView):
         return super().form_valid(form)
     def get_success_url(self):
         return reverse('accompany:accompany_detail', kwargs={'pk' : self.object.travel_id})
+    
+class AccompanyDeleteView(DeleteView):
+    model = TravelGroup
+    template_name = 'accompany/accompany_detail.html'
+    success_url = '/accompany/'
