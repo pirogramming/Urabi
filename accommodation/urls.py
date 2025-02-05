a801334727f3cb6d8c5fa986cefb800c261f2bd8
dhrkawk <jajwon202@naver.com>
2025-02-06 03:48:14 +0900

    [Feat] accommodation url 연결 [Fix] flash_detail 다른 번개 리스트 구글 api 반영

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'accommodation'
+app_name = "accommodation"
 
 urlpatterns = [
-    path('', views.accommodation_filter, name='filter'),
-    path('location/', views.location_view, name='location'),
+    path("", views.accommodation_filter, name="filter"), 
+    path("create/", views.accommodation_create, name="accommodation_create"), 
+    path("<int:pk>/", views.accommodation_review_detail, name="accommodation_review_detail"),  
+    path("location/", views.accommodation_location, name="accommodation_location"),  
 ]
