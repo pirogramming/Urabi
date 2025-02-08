0446a23a2d53bbcbb0a9a879e0d954277f9a494d
axoesnxm <sula9139@gmail.com>
2025-02-09 08:27:43 +0900

    [fix]asgi 버전 오류 수정, adapter 추가

--- config/asgi.py
+++ config/asgi.py
@@ -1,15 +1,17 @@
+# asgi.py
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
+import chat.routing  
+from config.asgi_adapter import ASGI3toASGI2Adapter 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
+django_asgi_app = get_asgi_application()
+django_asgi_app = ASGI3toASGI2Adapter(django_asgi_app)
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # ✅ HTTP 요청 처리 추가!
+    "http": django_asgi_app,  
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
---
+++ config/asgi_adapter.py
@@ -0,0 +1,12 @@
+# asgi_adapter.py
+import asyncio
+
+class ASGI3toASGI2Adapter:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, scope):
+        # 이 함수는 scope만 받고, receive와 send를 받는 callable(코루틴)을 반환합니다.
+        async def asgi3_app(receive, send):
+            await self.app(scope, receive, send)
+        return asgi3_app
