c40df10f898baf91d0d301f92e946af55e774efc
iseun15 <ave0315@naver.com>
2025-02-10 05:59:25 +0900

    [Refactor] 나눔마켓 최신순으로 정렬

--- market/forms.py
+++ market/forms.py
@@ -4,8 +4,30 @@ from django.core.exceptions import ValidationError
 
 class MarketForm(forms.ModelForm):
     class Meta:
-        model= Market
-        exclude = ('created_at', 'updated_at','user')
+        model = Market
+        exclude = ('created_at', 'updated_at', 'user')
+        widgets = {
+            'city': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '도시명을 입력하세요'
+            }),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '제목'
+            }),
+            'explanation': forms.Textarea(attrs={
+                'id': 'mkt_explanation',
+                'placeholder': '상품 설명을 입력하세요',
+                'rows': 5
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': '가격을 입력하세요'
+            }),
+            'photo': forms.ClearableFileInput(attrs={'class': 'file-upload-input', 'id':'image-upload'}),
+
+            
+        }
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
