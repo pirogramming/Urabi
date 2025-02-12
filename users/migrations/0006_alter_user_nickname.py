ee6359a55c3e5ff56b436221d704920e07a6cf38
03minseok <iris15977@gmail.com>
2025-02-12 22:08:25 +0900

    [design] accompany detail 스케줄 위치 변경

--- users/migrations/0006_alter_user_nickname.py
+++ users/migrations/0006_alter_user_nickname.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0005_alter_user_user_phone'),
+        ('users', '0001_initial'),
     ]
 
     operations = [
